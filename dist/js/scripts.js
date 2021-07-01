31bfdc90301a5fa2a5f3596b4800c973e2a1976d
Justin Golden <justingolden21@gmail.com>
2021-06-30 21:00:35 -1000

    add clock
    add clock with ability to programmatically set theme
    
    add  original theme and new theme
    
    next: adding options to change theme (as well as save multiple themes, reset settings, and save settings to local storage

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -8,5 +8,30 @@ if ('serviceWorker' in navigator) {
 /* global u */
 
 window.addEventListener('load', () => {
-	u('.btn').on('click', () => console.log('clicked'));
+	setTime();
+
+	setTheme(defaultTheme);
+	// setTheme(classicTheme);
 });
+
+function setTime() {
+	const date = new Date();
+
+	const h = date.getHours() % 12;
+	const m = date.getMinutes();
+	const s = date.getSeconds();
+
+	const sDegrees = 6 * s;
+	const mDegrees = (m + s / 60) * 6;
+	const hDegrees = (h + m / 60 + s / 3600) * 30;
+
+	u('#hour-hand')
+		.first()
+		.setAttribute('transform', `rotate(${hDegrees.toString()})`);
+	u('#minute-hand')
+		.first()
+		.setAttribute('transform', `rotate(${mDegrees.toString()})`);
+	u('#second-hand')
+		.first()
+		.setAttribute('transform', `rotate(${sDegrees.toString()})`);
+}
