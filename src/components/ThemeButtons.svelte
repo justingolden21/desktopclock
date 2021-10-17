79484c622472b946de92197ea9b66bcb0d9b55e1
RGB Studios <justingolden21@gmail.com>
2021-10-16 23:25:32 -0700

    active theme btn styles now work

--- src/components/ThemeButtons.svelte
+++ src/components/ThemeButtons.svelte
@@ -8,9 +8,20 @@
 <div>
 	{#each Object.keys(TailwindColors).slice(2) as color, colorIndex}
 		<button
-			class="theme-btn"
+			class="theme-btn {$session.settings.colorPalette == color ? 'active' : ''}"
 			style="background-color: {TailwindColors[color][300]}"
 			on:click={() => ($session.settings.colorPalette = color)}
 		/>
 	{/each}
 </div>
+
+<style>
+	/* purgecss start ignore */
+	.theme-btn {
+		@apply icon-btn w-6 h-6 m-1;
+	}
+	.theme-btn.active {
+		box-shadow: 4px 0px 0px #64748b; /* gray-500 */
+	}
+	/* purgecss end ignore */
+</style>
