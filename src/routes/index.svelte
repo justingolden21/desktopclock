36f80c711eb2dd354284058dba4e3527b4d7d2dc
RGB Studios <justingolden21@gmail.com>
2021-10-23 10:43:31 -0700

    move scripts.js to index.svelte
    scripts were only for analog clock rotation, which is only used in index (clock) anyway. can extrapolate this later. now we can subscribe to $now and use dayjs to set timezone of analog clock

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,68 @@
 
 	import { session } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { init } from '../js/scripts';
 
 	import Displays from '../components/Displays.svelte';
 	import ThemeButtons from '../components/ThemeButtons.svelte';
 
+	const setAngle = (type, newAngle) => {
+		document.documentElement.style.setProperty(`--${type}-angle`, `${newAngle}deg`);
+	};
+
+	// TODO BUG: timezone not refleced in analog clock
+	function setTime() {
+		if (!document.getElementById('hour-hand')) return; // return if analog clock is not visible
+
+		const date = new Date();
+
+		// todo: add one second to current date, because transition to current time takes one second
+		const h = date.getHours() % 12;
+		const m = date.getMinutes();
+		const s = date.getSeconds();
+
+		let rotations = {
+			second: 6 * s,
+			minute: (m + s / 60) * 6,
+			hour: (h + m / 60 + s / 3600) * 30
+		};
+
+		// check one second's rotation after 0 degrees
+		// because this is when we need to replace the angle with 0 and transition to the next second (rotation % 360)
+		if (rotations['second'] <= 6) rotations['second'] += 360;
+		if (rotations['minute'] <= 0.1) rotations['minute'] += 360;
+		if (rotations['hour'] <= 0.0084) rotations['hour'] += 360;
+
+		// Quickly reset position
+		// See https://stackoverflow.com/q/11131875/4907950
+		for (const handType of ['second', 'minute', 'hour']) {
+			setAngle(handType, rotations[handType]);
+
+			if (rotations[handType] > 360) {
+				let hand = document.getElementById(handType + '-hand');
+				hand.classList.add('notransition');
+				setAngle(handType, 0);
+				// See https://gist.github.com/paulirish/5d52fb081b3570c81e3a#svg
+				hand.getBBox(); // trigger CSS reflow
+				hand.classList.remove('notransition');
+				setAngle(handType, rotations[handType] % 360);
+			}
+		}
+	}
+
+	let setTimeInterval;
+
+	// manifest has to load before the service worker for the service worker to install
+	// since both are async here, the app breaks
+	// for localization, the manifest should be loaded from an endpoint in svelte kit, to point to a json that loads the correct language manifest
+
+	function init() {
+		setTime();
+		setTimeInterval = setInterval(setTime, 1 * 1000);
+	}
+
 	onMount(init);
+
+	//  ================
 
 	$: theme = $session.settings.clock.theme;
 	$: colorPalette = colors[$session.settings.colorPalette];
