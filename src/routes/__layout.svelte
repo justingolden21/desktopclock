fdc5f6416787a7cf5123de716a70429b4db6f274
Justin Golden <justingolden21@gmail.com>
2022-01-14 00:17:41 -0800

    clean hextorgb
    hex from tailwind will always be valid, remove validation for not matching hex regex

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -138,10 +138,7 @@
 	// https://stackoverflow.com/a/5624139/4907950
 	function hexToRgb(hex) {
 		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-		return result
-			? `${parseInt(result[1], 16)}, 
-			${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`
-			: null;
+		return `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
 	}
 
 	// store numbers as list of rgb values for use in withOpacity in tailwind.config.cjs
