a7703a0e4935e17eb01a851aefa4de0324d367d9
Justin Golden <justingolden21@gmail.com>
2022-01-13 18:57:58 -0800

    further clean theme btns
    remove wrapper div with relative class, unnecessary

--- src/components/ThemeButtons.svelte
+++ src/components/ThemeButtons.svelte
@@ -7,12 +7,10 @@
 	export let theme;
 </script>
 
-<div class="relative">
-	{#each colors as color}
-		<button
-			class="rounded-lg w-6 h-6 m-1 border-2"
-			style="background-color: {TailwindColors[color][300]};
+{#each colors as color}
+	<button
+		class="rounded-lg w-6 h-6 m-1 border-2"
+		style="background-color: {TailwindColors[color][300]};
             border-color: {$settings[theme] == color ? 'white' : TailwindColors[color][300]}"
-			on:click={() => ($settings[theme] = color)} />
-	{/each}
-</div>
+		on:click={() => ($settings[theme] = color)} />
+{/each}
