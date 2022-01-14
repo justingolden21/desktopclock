dcb44f76ae68be8e6995c993dd8e90f8d1d4b8a8
Justin Golden <justingolden21@gmail.com>
2022-01-13 21:48:34 -0800

    clean up loader

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -2,12 +2,13 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		document.querySelector('#loading-div').style.visibility = 'visible';
-		setTimeout(() => (document.querySelector('#loading-div').style.visibility = 'hidden'), 1000);
+		const loader = document.querySelector('#loader');
+		loader.style.visibility = 'visible';
+		setTimeout(() => (loader.style.visibility = 'hidden'), 1000);
 	});
 </script>
 
-<div id="loading-div" class="z-40 bg-gray-900 w-full h-full fixed">
+<div id="loader" class="z-40 bg-gray-900 w-full h-full fixed">
 	<div
 		class="absolute inset-1/2 w-16 h-16 rounded-full animate-spin border-8 border-white border-t-transparent" />
 </div>
