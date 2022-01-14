d51c13cf18a620f670033a811fa0065c7d7ff57e
Justin Golden <justingolden21@gmail.com>
2022-01-13 21:39:25 -0800

    show error message if lang is en
    also restyle error number from bold to normal (too many font sizes, fonts, font weights, etc)

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import { session } from '$app/stores';
+	import { settings } from '../components/settings.js';
 
 	export const load = ({ error, status }) => {
 		return {
@@ -25,10 +26,15 @@
 		<img src="img/404_undraw.svg" alt="" class="w-96 mx-auto my-8" />
 		<h2>{$session.languageDictionary.error['The resource could not be found']}</h2>
 	{/if}
-	<h2 class="font-bold my-4">{error.name} {status}</h2>
-	<!-- TODO: only show error.message if lang is english? -->
-	<p>{error.message}</p>
+	<h2 class="my-4">{error.name} {status}</h2>
+
+	<!-- show error message only if lang is english (error message is in english) -->
+	{#if $settings?.locale?.language?.substring(0, 2) == 'en'}
+		<p>{error.message}</p>
+	{:else}
+		<p>{$session.languageDictionary.error['Error']}</p>
+	{/if}
+
 	<a href="/" class="btn block mt-8 hover:no-underline"
-		>{$session.languageDictionary.error['Go back to the homepage']}</a
-	>
+		>{$session.languageDictionary.error['Go back to the homepage']}</a>
 </div>
