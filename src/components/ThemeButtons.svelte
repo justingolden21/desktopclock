ccd477d1cadc81f4b491b91f5cf1f4ec3f3c9420
Justin Golden <justingolden21@gmail.com>
2022-01-13 18:55:46 -0800

    clean theme btn css

--- src/components/ThemeButtons.svelte
+++ src/components/ThemeButtons.svelte
@@ -10,16 +10,9 @@
 <div class="relative">
 	{#each colors as color}
 		<button
-			class="theme-btn"
+			class="rounded-full w-6 h-6 m-1 border-2"
 			style="background-color: {TailwindColors[color][300]};
             border-color: {$settings[theme] == color ? 'white' : TailwindColors[color][300]}"
 			on:click={() => ($settings[theme] = color)} />
 	{/each}
 </div>
-
-<style lang="postcss">
-	.theme-btn {
-		@apply p-2 rounded-full hover:bg-gray-700 hover:text-white w-6 h-6 m-1 border-2; /* fix since it cannot find 'icon-btn' class in prod */
-		/* @apply icon-btn w-6 h-6 m-1; */
-	}
-</style>
