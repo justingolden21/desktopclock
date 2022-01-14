e6b0d6f11281d762ba27fe0bf3b1a5ec3b55069a
Justin Golden <justingolden21@gmail.com>
2022-01-13 21:47:44 -0800

    clean up loader

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -7,17 +7,7 @@
 	});
 </script>
 
-<div id="loading-div">
-	<div class="loader" />
+<div id="loading-div" class="z-40 bg-gray-900 w-full h-full fixed">
+	<div
+		class="absolute inset-1/2 w-16 h-16 rounded-full animate-spin border-8 border-white border-t-transparent" />
 </div>
-
-<style lang="postcss">
-	.loader {
-		@apply absolute inset-1/2 w-16 h-16 rounded-full animate-spin;
-		@apply border-8 border-white border-t-transparent;
-	}
-
-	#loading-div {
-		@apply z-40 bg-gray-900 w-full h-full fixed top-0 left-0 block;
-	}
-</style>
