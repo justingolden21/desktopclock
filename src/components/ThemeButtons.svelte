9aaca7906ea719a6fb061328ce30bc4a61c7d495
Justin Golden <justingolden21@gmail.com>
2022-01-13 18:53:29 -0800

    fix active class in theme buttons
    wasn't applying because border color was being overriden by inline style previously

--- src/components/ThemeButtons.svelte
+++ src/components/ThemeButtons.svelte
@@ -10,10 +10,9 @@
 <div class="relative">
 	{#each colors as color}
 		<button
-			class="theme-btn {$settings[theme] == color ? 'active' : ''}"
-			style="background-color: {TailwindColors[color][300]}; border-color: {TailwindColors[
-				color
-			][300]}"
+			class="theme-btn"
+			style="background-color: {TailwindColors[color][300]};
+            border-color: {$settings[theme] == color ? 'white' : TailwindColors[color][300]}"
 			on:click={() => ($settings[theme] = color)} />
 	{/each}
 </div>
@@ -22,8 +21,5 @@
 	.theme-btn {
 		@apply p-2 rounded-full hover:bg-gray-700 hover:text-white w-6 h-6 m-1 border-2; /* fix since it cannot find 'icon-btn' class in prod */
 		/* @apply icon-btn w-6 h-6 m-1; */
-	}
-	.theme-btn.active {
-		@apply border-white;
 	}
 </style>
