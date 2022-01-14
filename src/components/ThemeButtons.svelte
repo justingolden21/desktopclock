14eaefffa3b2d115eae4ab19ce9f04729c88c0fc
Justin Golden <justingolden21@gmail.com>
2022-01-13 18:59:31 -0800

    theme btn bg is 400 not 300 color

--- src/components/ThemeButtons.svelte
+++ src/components/ThemeButtons.svelte
@@ -10,7 +10,7 @@
 {#each colors as color}
 	<button
 		class="rounded-lg w-6 h-6 m-1 border-2"
-		style="background-color: {TailwindColors[color][300]};
+		style="background-color: {TailwindColors[color][400]};
             border-color: {$settings[theme] == color ? 'white' : TailwindColors[color][300]}"
 		on:click={() => ($settings[theme] = color)} />
 {/each}
