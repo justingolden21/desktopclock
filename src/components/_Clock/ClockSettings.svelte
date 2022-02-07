54f1240ce6af8b49ac8c3410da40829bad176425
Justin Golden <justingolden21@gmail.com>
2022-02-06 20:03:35 -0800

    Remove unused import

--- src/components/_Clock/ClockSettings.svelte
+++ src/components/_Clock/ClockSettings.svelte
@@ -2,7 +2,6 @@
 	import { session } from '$app/stores';
 	import { onMount } from 'svelte';
 
-	import TailwindColors from 'tailwindcss/colors.js';
 	import dayjs from 'dayjs';
 
 	import { now } from '../../util/now.js';
