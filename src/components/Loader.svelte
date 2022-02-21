e91b6d949dc3ca059bc6ba8d664d4e09a497029d
Justin Golden <justingolden21@gmail.com>
2022-02-21 13:28:34 -0800

    Update Loader.svelte
    now has light theme loader, also centered with -ml-8 (-mr-8 also works but not positive numbers)

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -10,8 +10,11 @@
 </script>
 
 {#if loading}
-	<div transition:fade={{ duration: 500 }} id="loader" class="z-40 bg-base-900 w-full h-full fixed">
+	<div
+		transition:fade={{ duration: 500 }}
+		id="loader"
+		class="z-40 bg-base-100 dark:bg-base-900 w-full h-full fixed">
 		<div
-			class="absolute inset-1/2 w-16 h-16 rounded-full animate-spin border-8 border-white border-t-transparent" />
+			class="absolute inset-1/2 w-16 h-16 -ml-8 rounded-full animate-spin border-8 border-base-900 border-t-transparent dark:border-base-100 dark:border-t-transparent" />
 	</div>
 {/if}
