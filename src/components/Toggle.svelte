50e80e5f07b6bf1e010446ff110fa9bf511de48a
Justin Golden <justingolden21@gmail.com>
2022-02-21 00:05:40 -0800

    move toggles 1 unit right (for focus ring)

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -5,11 +5,11 @@
 </script>
 
 <div class="block">
-	<div class="relative inline-block w-12 m-2 ml-0 align-middle select-none">
+	<div class="relative inline-block w-12 m-2 ml-1 align-middle select-none">
 		<input bind:checked type="checkbox" {id} class="toggle-checkbox" on:change />
 		<label for={id} class="toggle-bg" />
 	</div>
-	<label for={id} class="hover:text-base-500 dark:hover:text-base-400">
+	<label for={id} class="hover:text-base-500 dark:hover:text-base-400 select-none">
 		{labelText}
 	</label>
 </div>
