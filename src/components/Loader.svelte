66544f7d744a1c36944b1403960385a1f61c837e
Justin Golden <justingolden21@gmail.com>
2022-02-21 13:21:02 -0800

    Update Loader.svelte
    fades in now, tested with buildpreview for a while and this seems to work. takes about 1s before analog clock moves, works well in digital as well, and night and day modes

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -1,14 +1,17 @@
 <script>
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	let loading = true;
 
 	onMount(() => {
-		const loader = document.querySelector('#loader');
-		loader.style.visibility = 'visible';
-		setTimeout(() => (loader.style.visibility = 'hidden'), 1000);
+		setTimeout(() => (loading = false), 500);
 	});
 </script>
 
-<div id="loader" class="z-40 bg-base-900 w-full h-full fixed">
-	<div
-		class="absolute inset-1/2 w-16 h-16 rounded-full animate-spin border-8 border-white border-t-transparent" />
-</div>
+{#if loading}
+	<div transition:fade={{ duration: 500 }} id="loader" class="z-40 bg-base-900 w-full h-full fixed">
+		<div
+			class="absolute inset-1/2 w-16 h-16 rounded-full animate-spin border-8 border-white border-t-transparent" />
+	</div>
+{/if}
