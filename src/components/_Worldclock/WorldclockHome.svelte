916458eb4ffc855c036879a2226c5bf39855c6d6
Justin Golden <justingolden21@gmail.com>
2022-02-21 21:44:47 -0800

    Update WorldclockHome.svelte

--- src/components/_Worldclock/WorldclockHome.svelte
+++ src/components/_Worldclock/WorldclockHome.svelte
@@ -5,9 +5,9 @@
 
 <div class="grid grid-cols-2 gap-4 sm:gap-8">
 	<div class="text-left">
-		<p>{$date}</p>
-		<p>{$time}</p>
 		<p>{$timezone}</p>
+		<p class="font-bold text-4xl">{$time}</p>
+		<p>{$date}</p>
 		<p>UTC {$utcOffset}</p>
 	</div>
 	<div>
