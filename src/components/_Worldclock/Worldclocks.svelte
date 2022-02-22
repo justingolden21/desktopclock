5a572310d6d61cd1bd91880ce358eb66dfe499e2
Justin Golden <justingolden21@gmail.com>
2022-02-21 20:50:44 -0800

    update worldclocks grid

--- src/components/_Worldclock/Worldclocks.svelte
+++ src/components/_Worldclock/Worldclocks.svelte
@@ -14,19 +14,19 @@
 	};
 </script>
 
-<div class="grid grid-cols-2 gap-4 sm:gap-8">
-	{#each worldclocks.timezones as timezone}
-		<div class="text-left border-y-2 border-base-500">
+{#each worldclocks.timezones as timezone}
+	<div class="grid grid-cols-2 gap-4 sm:gap-8 border-y-2 border-base-500">
+		<div class="text-left">
 			<p>{$getDate(timezone)}</p>
 			<p>{$getTime(timezone)}</p>
 			<p>{timezone.split('_').join(' ')}</p>
 			<p>UTC {$getUtcOffset(timezone)}</p>
 			<p>{$getHourDiff(timezone)}</p>
 		</div>
-		<div class="border-y-2 border-base-500">
+		<div>
 			<div class="w-32 h-32 relative">
 				<AnalogClock mode="worldclock" {timezone} />
 			</div>
 		</div>
-	{/each}
-</div>
+	</div>
+{/each}
