2ac1ee83fdd78ad55812c8a12fd207b1a76d4db5
Justin Golden <justingolden21@gmail.com>
2022-02-21 23:43:35 -0800

    move dropdown btn outside

--- src/components/_Worldclock/Worldclocks.svelte
+++ src/components/_Worldclock/Worldclocks.svelte
@@ -61,29 +61,29 @@
 			<div class="w-24 h-24 sm:w-32 sm:h-32 relative ml-auto mr-6">
 				<AnalogClock mode="worldclock" {timezone} />
 			</div>
-			<div class="absolute top-2 right-2">
-				<button
-					id="dropdown-btn"
-					class="icon-btn"
-					on:click={() => (dropdownOpen = !dropdownOpen)}
-					aria-label={$session.languageDictionary.labels['Menu']}>
-					<Icon name="dots_vertical" class="w-6 h-6" />
-				</button>
+		</div>
+		<div class="absolute top-2 right-2">
+			<button
+				id="dropdown-btn"
+				class="icon-btn"
+				on:click={() => (dropdownOpen = !dropdownOpen)}
+				aria-label={$session.languageDictionary.labels['Menu']}>
+				<Icon name="dots_vertical" class="w-6 h-6" />
+			</button>
 
-				<!-- dropdown menu -->
-				<ul
-					class="{dropdownOpen ? '' : 'hidden'} w-40 rounded shadow surface mt-2"
-					aria-labelledby="dropdown-btn">
-					<!-- TODO translate -->
-					{#each 'Edit Up Down Delete'.split(' ') as txt}
-						<li
-							class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
-							<Icon name="close" class="inline-block w-6 h-6" />
-							{txt}
-						</li>
-					{/each}
-				</ul>
-			</div>
+			<!-- dropdown menu -->
+			<ul
+				class="{dropdownOpen ? '' : 'hidden'} w-40 rounded shadow surface mt-2"
+				aria-labelledby="dropdown-btn">
+				<!-- TODO translate -->
+				{#each 'Edit Up Down Delete'.split(' ') as txt}
+					<li
+						class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
+						<Icon name="close" class="inline-block w-6 h-6" />
+						{txt}
+					</li>
+				{/each}
+			</ul>
 		</div>
 	</div>
 {/each}
