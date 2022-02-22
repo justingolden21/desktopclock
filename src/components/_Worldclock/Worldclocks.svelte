93e43ae3dbf656bab74b885279d5c969f454e6fc
Justin Golden <justingolden21@gmail.com>
2022-02-21 23:42:03 -0800

    remove unused div

--- src/components/_Worldclock/Worldclocks.svelte
+++ src/components/_Worldclock/Worldclocks.svelte
@@ -71,18 +71,18 @@
 				</button>
 
 				<!-- dropdown menu -->
-				<div class="{dropdownOpen ? '' : 'hidden'} w-40 rounded shadow surface mt-2">
-					<ul aria-labelledby="dropdown-btn">
-						<!-- TODO translate -->
-						{#each 'Edit Up Down Delete'.split(' ') as txt}
-							<li
-								class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
-								<Icon name="close" class="inline-block w-6 h-6" />
-								{txt}
-							</li>
-						{/each}
-					</ul>
-				</div>
+				<ul
+					class="{dropdownOpen ? '' : 'hidden'} w-40 rounded shadow surface mt-2"
+					aria-labelledby="dropdown-btn">
+					<!-- TODO translate -->
+					{#each 'Edit Up Down Delete'.split(' ') as txt}
+						<li
+							class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
+							<Icon name="close" class="inline-block w-6 h-6" />
+							{txt}
+						</li>
+					{/each}
+				</ul>
 			</div>
 		</div>
 	</div>
