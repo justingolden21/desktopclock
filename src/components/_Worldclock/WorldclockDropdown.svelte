ee21ea6992b5266e17a213aef8c6848b965eb3f1
Justin Golden <justingolden21@gmail.com>
2022-02-22 20:38:40 -0800

    worldclock dropdown component

---
+++ src/components/_Worldclock/WorldclockDropdown.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { session } from '$app/stores';
+
+	import Icon from '../Icon.svelte';
+
+	let dropdownOpen = false;
+</script>
+
+<div class="absolute top-2 right-2 z-10">
+	<button
+		id="dropdown-btn"
+		class="icon-btn ml-auto block"
+		on:click={() => (dropdownOpen = !dropdownOpen)}
+		aria-label={$session.languageDictionary.labels['Menu']}>
+		<Icon name="dots_vertical" class="w-6 h-6" />
+	</button>
+
+	<ul
+		class="{dropdownOpen ? '' : 'hidden'} w-40 rounded surface mt-2"
+		aria-labelledby="dropdown-btn">
+		<!-- TODO translate -->
+		{#each 'Edit Up Down Delete'.split(' ') as txt}
+			<li>
+				<!-- svelte-ignore a11y-invalid-attribute -->
+				<a
+					href=""
+					on:click={() => (dropdownOpen = false)}
+					class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
+					<Icon name="close" class="inline-block w-6 h-6" />
+					{txt}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/components/_Worldclock/Worldclocks.svelte
+++ src/components/_Worldclock/Worldclocks.svelte
@@ -1,8 +1,6 @@
 <script>
-	import { session } from '$app/stores';
-
-	import Icon from '../Icon.svelte';
 	import AnalogClock from '../_Clock/AnalogClock.svelte';
+	import WorldclockDropdown from './WorldclockDropdown.svelte';
 
 	import {
 		time,
@@ -25,8 +23,6 @@
 		],
 		style: 'rows'
 	};
-
-	let dropdownOpen = true;
 </script>
 
 <!-- Home clock -->
@@ -64,33 +60,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="absolute top-2 right-2 z-10">
-			<button
-				id="dropdown-btn"
-				class="icon-btn ml-auto block"
-				on:click={() => (dropdownOpen = !dropdownOpen)}
-				aria-label={$session.languageDictionary.labels['Menu']}>
-				<Icon name="dots_vertical" class="w-6 h-6" />
-			</button>
-			<!-- dropdown menu -->
-			<ul
-				class="{dropdownOpen ? '' : 'hidden'} w-40 rounded surface mt-2"
-				aria-labelledby="dropdown-btn">
-				<!-- TODO translate -->
-				{#each 'Edit Up Down Delete'.split(' ') as txt}
-					<li>
-						<!-- svelte-ignore a11y-invalid-attribute -->
-						<a
-							href=""
-							on:click={() => (dropdownOpen = false)}
-							class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
-							<Icon name="close" class="inline-block w-6 h-6" />
-							{txt}
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
+		<WorldclockDropdown />
 	</div>
 {/each}
 
@@ -106,11 +76,7 @@
 				UTC {$getUtcOffset(timezone)}
 				(<span class="font-bold">{$getHourDiff(timezone)}</span>)
 			</p>
-			<button
-				class="icon-btn absolute top-2 right-2"
-				aria-label={$session.languageDictionary.labels['Menu']}>
-				<Icon name="dots_vertical" class="w-6 h-6" />
-			</button>
+			<WorldclockDropdown />
 		</div>
 	{/each}
 
@@ -127,11 +93,7 @@
 				UTC {$getUtcOffset(timezone)}
 				(<span class="font-bold">{$getHourDiff(timezone)}</span>)
 			</p>
-			<button
-				class="icon-btn absolute top-2 right-2"
-				aria-label={$session.languageDictionary.labels['Menu']}>
-				<Icon name="dots_vertical" class="w-6 h-6" />
-			</button>
+			<WorldclockDropdown />
 		</div>
 	{/each}
 </div>
