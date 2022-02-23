72b072ea7b7acac5cae215e262e01b36518f0bf4
Justin Golden <justingolden21@gmail.com>
2022-02-22 20:57:03 -0800

    add icons for worldclock options
    renamed chevron right to use underscore to be consistent (and updated accordion panel)

--- src/components/_Worldclock/WorldclockDropdown.svelte
+++ src/components/_Worldclock/WorldclockDropdown.svelte
@@ -4,6 +4,25 @@
 	import Icon from '../Icon.svelte';
 
 	let dropdownOpen = false;
+
+	const options = [
+		{
+			text: 'Edit',
+			icon: 'pencil'
+		},
+		{
+			text: 'Up',
+			icon: 'chevron_up'
+		},
+		{
+			text: 'Down',
+			icon: 'chevron_down'
+		},
+		{
+			text: 'Delete',
+			icon: 'trash'
+		}
+	];
 </script>
 
 <div class="absolute top-2 right-2 z-10">
@@ -18,15 +37,15 @@
 	<ul
 		class="{dropdownOpen ? '' : 'hidden'} w-40 rounded surface mt-2"
 		aria-labelledby="dropdown-btn">
-		{#each 'Edit Up Down Delete'.split(' ') as txt}
+		{#each options as option}
 			<li>
 				<!-- svelte-ignore a11y-invalid-attribute -->
 				<a
 					href=""
 					on:click={() => (dropdownOpen = false)}
-					class="block cursor-pointer py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
-					<Icon name="close" class="inline-block w-6 h-6" />
-					{$session.languageDictionary.labels[txt]}
+					class="block cursor-pointer hover:no-underline py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
+					<Icon name={option.icon} class="inline-block w-6 h-6" />
+					{$session.languageDictionary.labels[option.text]}
 				</a>
 			</li>
 		{/each}
