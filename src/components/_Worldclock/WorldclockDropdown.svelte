dfaedc5263877d3e842619bd137628eb7e0bfc4c
Justin Golden <justingolden21@gmail.com>
2022-02-24 00:39:15 -0800

    change dropdown a to btn

--- src/components/_Worldclock/WorldclockDropdown.svelte
+++ src/components/_Worldclock/WorldclockDropdown.svelte
@@ -45,17 +45,15 @@
 			<!-- don't show up if first or down if last -->
 			{#if !((option.text === 'Up' && idx === 0) || (option.text === 'Down' && idx === $settings.worldclock.timezones.length - 1))}
 				<li>
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a
-						href=""
+					<button
 						on:click={() => {
 							dropdownOpen = false;
 							option.func();
 						}}
-						class="block cursor-pointer hover:no-underline py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
+						class="w-full py-2 px-4 text-sm font-bold text-left hover:bg-base-300 hover:bg-opacity-75 dark:hover:bg-base-600 dark:hover:bg-opacity-75">
 						<Icon name={option.icon} class="inline-block w-6 h-6 mr-2" />
 						{$session.languageDictionary.labels[option.text]}
-					</a>
+					</button>
 				</li>
 			{/if}
 		{/each}
