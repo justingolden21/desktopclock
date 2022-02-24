877101976f50d936401c2c5fc4f08d3b7d70ccd0
Justin Golden <justingolden21@gmail.com>
2022-02-23 22:37:45 -0800

    ok NOW the tray width works
    GOOD LORD

--- src/components/_Worldclock/WorldclockTray.svelte
+++ src/components/_Worldclock/WorldclockTray.svelte
@@ -15,7 +15,7 @@
 	// by having these strings in the code, these utility classes are generated
 	// they are dynamically applied to the nav below
 	// in order to make its width the remaining space on the page
-	const unused_vars = ['w-[calc(100%_-_18rem)]', 'w-[calc(100%_-_8rem)]'];
+	const unused_vars = ['md:w-[calc(100%_-_18rem)]', 'md:w-[calc(100%_-_8rem)]'];
 </script>
 
 <!-- Similar classes to nav in nav.svelte -->
@@ -36,6 +36,7 @@
     h-16
     grid
     grid-cols-4
+    w-full
     {$settings.alwaysCollapseMenu || isFullscreen
 		? ''
 		: $settings.smallerMenu
@@ -43,7 +44,7 @@
 		: 'md:ml-72'}
     {$settings.alwaysCollapseMenu || isFullscreen
 		? ''
-		: 'w-[calc(100%_-_' + ($settings.smallerMenu ? 32 / 4 : 72 / 4) + 'rem)]'}">
+		: 'md:w-[calc(100%_-_' + ($settings.smallerMenu ? 32 / 4 : 72 / 4) + 'rem)]'}">
 	<button class="icon-btn mx-auto" on:click={() => 0} aria-label={''} title={''}>
 		<Icon name="plus" class="w-8 h-8" />
 	</button>
