50da509e1bcd124e459d293c91f36c376ae5d4ed
Justin Golden <justingolden21@gmail.com>
2022-02-23 22:07:18 -0800

    worldclock tray resizes based on nav size

--- src/components/_Worldclock/WorldclockTray.svelte
+++ src/components/_Worldclock/WorldclockTray.svelte
@@ -1,5 +1,16 @@
 <script>
+	import Screenfull from 'screenfull';
+	import { onMount } from 'svelte';
+
+	import { settings } from '../settings.js';
 	import Icon from '../Icon.svelte';
+
+	// similar to header
+	let isFullscreen;
+	onMount(() => {
+		isFullscreen = Screenfull.isFullscreen;
+		Screenfull.on('change', () => (isFullscreen = Screenfull.isFullscreen));
+	});
 </script>
 
 <!-- Similar classes to nav in nav.svelte -->
@@ -15,7 +26,12 @@
     border-t-2
     dark:bg-base-800
     dark:bg-opacity-75
-    h-16">
+    h-16
+    {$settings.alwaysCollapseMenu || isFullscreen
+		? ''
+		: $settings.smallerMenu
+		? 'ml-32'
+		: 'ml-72'}">
 	<button class="icon-btn px-24" on:click={() => 0} aria-label={''} title={''}>
 		<Icon name="plus" class="w-8 h-8" />
 	</button>
