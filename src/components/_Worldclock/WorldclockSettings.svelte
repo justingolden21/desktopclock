30cb100e815a4299fec264ec3348a6fa40d8e590
Justin Golden <justingolden21@gmail.com>
2022-02-24 20:36:08 -0800

    add worldclock settings btns

--- src/components/_Worldclock/WorldclockSettings.svelte
+++ src/components/_Worldclock/WorldclockSettings.svelte
@@ -1,10 +1,40 @@
 <script>
 	import { session } from '$app/stores';
 
+	import { settings, defaultSettings } from '../settings.js';
+	import Icon from '../Icon.svelte';
+
 	$: dictionary = $session.languageDictionary;
 </script>
 
 <p>{dictionary.messages['Coming soon']}</p>
+
+<button
+	class="btn undo-btn block mt-4"
+	on:click={() => {
+		$settings.worldclock.timezones = defaultSettings.worldclock.timezones;
+	}}>
+	<Icon name="undo" class="inline w-6 h-6" />
+	{dictionary.worldclockSettings['Reset timezones']}
+</button>
+
+<button
+	class="btn trash-btn block mt-4"
+	on:click={() => {
+		$settings.worldclock.timezones = [];
+	}}>
+	<Icon name="trash" class="inline w-6 h-6" />
+	{dictionary.worldclockSettings['Delete timezones']}
+</button>
+
+<button
+	class="btn undo-btn block mt-4"
+	on:click={() => {
+		$settings.worldclock = defaultSettings.worldclock;
+	}}>
+	<Icon name="undo" class="inline w-6 h-6" />
+	{dictionary.worldclockSettings['Reset worldclock settings']}
+</button>
 
 <!-- 
 primary display
