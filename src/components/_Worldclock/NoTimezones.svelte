492ddee94df386da837e3fb38fa5a434cd10598f
Justin Golden <justingolden21@gmail.com>
2022-03-03 16:53:03 -0800

    clean up notimezones to use new modal component

--- src/components/_Worldclock/NoTimezones.svelte
+++ src/components/_Worldclock/NoTimezones.svelte
@@ -1,13 +1,10 @@
 <script>
 	import { session } from '$app/stores';
 
-	import Modal from '../Modal.svelte';
+	import { open } from '../../util/modal.js';
 	import Icon from '../Icon.svelte';
-	import NewWorldclock from './NewWorldclock.svelte';
 
 	$: dictionary = $session.languageDictionary;
-
-	let newWorldclockModal;
 </script>
 
 <img src="img/time_management_undraw.svg" alt="" class="w-72 mx-auto mt-4" />
@@ -17,16 +14,9 @@
 <!-- copied from worldclock tray: -->
 <button
 	class="icon-btn rounded-none bg-transparent mx-auto"
-	on:click={() => newWorldclockModal.show()}
+	on:click={() => open('new-world-clock')}
 	aria-label={dictionary.worldclockSettings['New timezone']}
 	title={dictionary.worldclockSettings['New timezone']}>
 	<Icon name="plus" class="inline-block w-8 h-8" />
 	<span>{dictionary.labels['New']}</span>
 </button>
-
-<Modal
-	bind:this={newWorldclockModal}
-	title={$session.languageDictionary.worldclockSettings['New timezone']}
-	icon="plus_circle">
-	<NewWorldclock bind:modal={newWorldclockModal} />
-</Modal>
