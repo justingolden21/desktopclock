edfc9b937fdfcdd39d1801f0e75141daa1787172
Justin Golden <justingolden21@gmail.com>
2022-03-05 14:41:08 -0800

    Update WorldclockTimetable.svelte
    an attempt was made. will keep improving over time

--- src/components/_Worldclock/WorldclockTimetable.svelte
+++ src/components/_Worldclock/WorldclockTimetable.svelte
@@ -16,24 +16,82 @@
 		getUtcOffset,
 		getHourDiff
 	} from '../../util/timeText';
+
+	// let times = ['9:00', '12:00', '15:00', '18:00'];
+	let times = [
+		{ hours: 9, mins: 0 },
+		{ hours: 12, mins: 0 },
+		{ hours: 15, mins: 0 },
+		{ hours: 18, mins: 0 }
+	];
+	let ampm = false;
+
+	// in mins
+	function getDiff(time1, time2) {
+		console.log(time1);
+		console.log(time2);
+		if (typeof time1 === 'string') time1 = timeStrToObj(time1);
+		if (typeof time2 === 'string') time2 = timeStrToObj(time2);
+
+		const mins1 = objToMins(time1);
+		const mins2 = objToMins(time2);
+
+		const minsDiff = mins1 - mins2;
+
+		return minsToTimeStr(minsDiff, true);
+	}
+	function timeStrToObj(timeStr) {
+		const pm = timeStr.toUpperCase().includes('pm');
+		const hours = parseInt(timeStr.split(':')[0]) + (pm ? 12 : 0);
+		const mins = parseInt(timeStr.split(':')[1]);
+		return { hours, mins };
+	}
+	function timeStrToMins(timeStr) {
+		return objToMins(timeStrToObj(timeStr));
+	}
+	function minsToObj(n) {
+		const h = n / 60;
+		const hours = Math.floor(h);
+		const m = (h - hours) * 60;
+		const mins = Math.round(m);
+		return {
+			hours,
+			mins
+		};
+	}
+	function objToTimeStr(obj, ampm = false) {
+		let h = obj.hours % (ampm ? 12 : 24);
+		if (ampm && h === 0) h = 12;
+		const m = obj.mins.toString().padStart(2, '0');
+		return `${h}:${m}${ampm ? (obj.hours >= 12 ? ' PM' : ' AM') : ''}`;
+	}
+	function minsToTimeStr(n, ampm = false) {
+		return objToTimeStr(minsToObj(n), ampm);
+	}
+	function objToMins(obj) {
+		return obj.hours * 60 + obj.mins;
+	}
 </script>
 
 <!-- TODO: translate diff, name, now, home -->
 <table>
 	<thead>
 		<tr>
+			<th />
 			<th>Difference</th>
 			<th>Name</th>
 			<th>Now</th>
-			<th><TimePicker /></th>
-			<th><TimePicker /></th>
-			<th><TimePicker /></th>
-			<th><TimePicker /></th>
+			{#each times as { hours, mins }}
+				<th><TimePicker {ampm} bind:hours bind:mins /></th>
+			{/each}
 		</tr>
 	</thead>
 	<tbody>
 		{#each [{ name: 'Home', zone: $settings.locale.timezone }].concat($settings.worldclock.timezones) as timezone, idx}
 			<tr>
+				<td class="relative w-12 h-12">
+					<WorldclockDropdown {idx} />
+				</td>
 				<td>
 					{#if idx !== 0}
 						{$getHourDiff(timezone.zone)}
@@ -48,13 +106,17 @@
 					<!-- <TimezoneSelect /> -->
 				</td>
 				<td class:font-bold={idx === 0}>{$getTime(timezone.zone)}</td>
-				<td>1:00</td>
-				<td>2:00</td>
-				<td>3:00</td>
-				<td>4:00</td>
+				{#each times as time}
+					<!-- timetext
+					get diff between time selector and current time
+				then add diff to timezone time -->
+					<td
+						>{getDiff(
+							getDiff($getTime($settings.locale.timezone), $getTime(timezone.zone)),
+							time
+						)}</td>
+				{/each}
 			</tr>
-
-			<WorldclockDropdown {idx} />
 		{/each}
 	</tbody>
 </table>
