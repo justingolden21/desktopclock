99c189e41d0ddc1780b83c8c3c9eaadad63e3740
Justin Golden <justingolden21@gmail.com>
2022-03-06 12:22:58 -0800

    simplify simple time picker ampm logic

--- src/components/SimpleTimePicker.svelte
+++ src/components/SimpleTimePicker.svelte
@@ -3,21 +3,16 @@
 	export let ampm = false;
 
 	const times = [];
-	if (ampm) {
-		for (let i = 0; i < 24 * 4; i++) {
-			const h = Math.floor(i / 4) % 12;
-			const str =
-				(h === 0 ? 12 : h) +
-				':' +
-				((i % 4) * 15).toString().padStart(2, '0') +
-				(i >= 24 * 2 ? ' PM' : ' AM');
-			times.push(str);
-		}
-	} else {
-		for (let i = 0; i < 24 * 4; i++) {
-			const str = Math.floor(i / 4) + ':' + ((i % 4) * 15).toString().padStart(2, '0');
-			times.push(str);
-		}
+
+	for (let i = 0; i < 24 * 4; i++) {
+		let h = Math.floor(i / 4);
+		if (ampm) h %= 12;
+		const str =
+			(ampm && h === 0 ? 12 : h) +
+			':' +
+			((i % 4) * 15).toString().padStart(2, '0') +
+			(ampm ? (i >= 24 * 2 ? ' PM' : ' AM') : '');
+		times.push(str);
 	}
 </script>
 
