e25d53f3e005e7fe9cd48e675390f6d48e77debb
Justin Golden <justingolden21@gmail.com>
2022-03-06 20:53:31 -0800

    Create ToggleDisplays.svelte

---
+++ src/components/ToggleDisplays.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { page } from '$app/stores';
+	import { settings } from './settings';
+
+	function togglePrimaryDisplay() {
+		if ($page.url.pathname === '/') {
+			const options = ['analog', 'time', 'date', 'datetime'];
+			$settings.clock.displays.primary =
+				options[(options.indexOf($settings.clock.displays.primary) + 1) % options.length];
+		} else if ($page.url.pathname === '/worldclock') {
+			const options = ['analog_digital', 'digital'];
+			$settings.worldclock.displays.primary =
+				options[(options.indexOf($settings.worldclock.displays.primary) + 1) % options.length];
+		}
+	}
+	function toggleSecondaryDisplay() {
+		if ($page.url.pathname === '/') {
+			const options = ['time', 'date', 'datetime', 'none'];
+			$settings.clock.displays.secondary =
+				options[(options.indexOf($settings.clock.displays.secondary) + 1) % options.length];
+		} else if ($page.url.pathname === '/worldclock') {
+			const options = ['rows', 'analog_grid', 'digital_grid'];
+			$settings.worldclock.displays.secondary =
+				options[(options.indexOf($settings.worldclock.displays.secondary) + 1) % options.length];
+		}
+	}
+
+	export { togglePrimaryDisplay, toggleSecondaryDisplay };
+</script>
