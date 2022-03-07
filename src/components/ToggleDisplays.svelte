389b5017b16efeb3bd1e2ef59d9c20dae1549634
Justin Golden <justingolden21@gmail.com>
2022-03-06 20:55:25 -0800

    Update ToggleDisplays.svelte

--- src/components/ToggleDisplays.svelte
+++ src/components/ToggleDisplays.svelte
@@ -4,25 +4,22 @@
 
 	function togglePrimaryDisplay() {
 		if ($page.url.pathname === '/') {
-			const options = ['analog', 'time', 'date', 'datetime'];
-			$settings.clock.displays.primary =
-				options[(options.indexOf($settings.clock.displays.primary) + 1) % options.length];
+			toggleDisplay('clock', 'primary', ['analog', 'time', 'date', 'datetime']);
 		} else if ($page.url.pathname === '/worldclock') {
-			const options = ['analog_digital', 'digital'];
-			$settings.worldclock.displays.primary =
-				options[(options.indexOf($settings.worldclock.displays.primary) + 1) % options.length];
+			toggleDisplay('worldclock', 'primary', ['analog_digital', 'digital']);
 		}
 	}
 	function toggleSecondaryDisplay() {
 		if ($page.url.pathname === '/') {
-			const options = ['time', 'date', 'datetime', 'none'];
-			$settings.clock.displays.secondary =
-				options[(options.indexOf($settings.clock.displays.secondary) + 1) % options.length];
+			toggleDisplay('clock', 'secondary', ['time', 'date', 'datetime', 'none']);
 		} else if ($page.url.pathname === '/worldclock') {
-			const options = ['rows', 'analog_grid', 'digital_grid'];
-			$settings.worldclock.displays.secondary =
-				options[(options.indexOf($settings.worldclock.displays.secondary) + 1) % options.length];
+			toggleDisplay('worldclock', 'secondary', ['rows', 'analog_grid', 'digital_grid']);
 		}
+	}
+
+	function toggleDisplay(page, displayType, options) {
+		$settings[page].displays[displayType] =
+			options[(options.indexOf($settings[page].displays[displayType]) + 1) % options.length];
 	}
 
 	export { togglePrimaryDisplay, toggleSecondaryDisplay };
