128aac516d304a308e71bb4af8cfcd72700644ca
Justin Golden <justingolden21@gmail.com>
2022-03-26 14:01:25 -0700

    toggle modal code works now

--- src/util/modal.js
+++ src/util/modal.js
@@ -4,6 +4,6 @@ import { writable, get } from 'svelte/store';
 
 export const modal = writable({});
 
-export const close = () => modal.set(null);
+export const close = () => modal.set({});
 export const open = (name, data = {}) => modal.set({ name, data });
-export const toggle = (name, data = {}) => (get(modal) ? open(name, data) : close());
+export const toggle = (name, data = {}) => (!get(modal)?.name ? open(name, data) : close());
