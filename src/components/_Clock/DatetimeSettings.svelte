f31aeb4ca77f71c09d80860be2da68f7b0250e8a
Justin Golden <justingolden21@gmail.com>
2022-03-27 13:56:55 -0700

    datetime settings uses page param

--- src/components/_Clock/DatetimeSettings.svelte
+++ src/components/_Clock/DatetimeSettings.svelte
@@ -23,7 +23,7 @@
 		<SettingSelect
 			id="time-format-select"
 			selectLabel={dictionary.clockSettings['Time format:']}
-			bind:value={$settings.clock.timeFormat}
+			bind:value={$settings[page].timeFormat}
 			values={defaultTimeFormats}
 			labelMapper={(timeFormat) =>
 				timeFormat === 'custom'
@@ -33,19 +33,19 @@
 							.locale($settings.locale.datetime)
 							.format?.(timeFormat)} />
 
-		{#if $settings.clock.timeFormat === 'custom'}
+		{#if $settings[page].timeFormat === 'custom'}
 			<div class="my-2 ml-8">
 				<input
 					type="text"
 					spellcheck="false"
 					class="block my-2"
-					bind:value={$settings.clock.timeFormatCustom} />
+					bind:value={$settings[page].timeFormatCustom} />
 				<p>
 					<b>{dictionary.clockSettings['Preview:']}</b>
 					{new dayjs($now)
 						.tz($settings.locale.timezone || 'Etc/GMT')
 						.locale($settings.locale.datetime)
-						.format?.($settings.clock.timeFormatCustom)}
+						.format?.($settings[page].timeFormatCustom)}
 				</p>
 			</div>
 		{/if}
@@ -55,7 +55,7 @@
 		<SettingSelect
 			id="date-format-select"
 			selectLabel={dictionary.clockSettings['Date format:']}
-			bind:value={$settings.clock.dateFormat}
+			bind:value={$settings[page].dateFormat}
 			values={defaultDateFormats}
 			labelMapper={(dateFormat) =>
 				dateFormat === 'custom'
@@ -65,25 +65,25 @@
 							.locale($settings.locale.datetime)
 							.format?.(dateFormat)} />
 
-		{#if $settings.clock.dateFormat === 'custom'}
+		{#if $settings[page].dateFormat === 'custom'}
 			<div class="my-2 ml-8">
 				<input
 					type="text"
 					spellcheck="false"
 					class="block my-2"
-					bind:value={$settings.clock.dateFormatCustom} />
+					bind:value={$settings[page].dateFormatCustom} />
 				<p>
 					<b>{dictionary.clockSettings['Preview:']}</b>
 					{new dayjs($now)
 						.tz($settings.locale.timezone || 'Etc/GMT')
 						.locale($settings.locale.datetime)
-						.format?.($settings.clock.dateFormatCustom)}
+						.format?.($settings[page].dateFormatCustom)}
 				</p>
 			</div>
 		{/if}
 	</div>
 
-	{#if $settings.clock.dateFormat === 'custom' || $settings.clock.timeFormat === 'custom'}
+	{#if $settings[page].dateFormat === 'custom' || $settings[page].timeFormat === 'custom'}
 		<button class="btn block my-2" on:click={() => open('datetime-format')}
 			>{dictionary.clockSettings['Custom formatting reference']}</button>
 	{/if}
@@ -92,17 +92,17 @@
 		class="btn undo-btn block"
 		on:click={() => {
 			for (const format of 'timeFormat timeFormatCustom dateFormat dateFormatCustom'.split(' '))
-				$settings.clock[format] = defaultSettings.clock[format];
+				$settings[page][format] = $settings[page][format];
 
 			// copied from `_layout.svelte` since `timeFormat` will be null
 			// because it's null in `defaultSettings` because it's overriden in `_layout.svelte` like so:
-			if ($settings.clock.timeFormat === null) {
+			if ($settings[page].timeFormat === null) {
 				// https://stackoverflow.com/q/27647918/4907950
 				const AMPM =
 					Intl.DateTimeFormat(navigator.language, { hour: 'numeric' }).resolvedOptions()
 						.hourCycle === 'h12';
-				$settings.clock.timeFormat = AMPM ? 'h:mm A' : 'H:mm';
-				$settings.clock.timeFormatCustom = AMPM ? 'h:mm A' : 'H:mm';
+				$settings[page].timeFormat = AMPM ? 'h:mm A' : 'H:mm';
+				$settings[page].timeFormatCustom = AMPM ? 'h:mm A' : 'H:mm';
 			}
 		}}>
 		<Icon name="undo" class="inline w-6 h-6" />
