5236c8ef23bda396752e85036c2bf55a5f5425f4
Justin Golden <justingolden21@gmail.com>
2022-04-14 17:06:49 -0700

    clean up digital displays divs, bottom alignment

--- src/components/_Clock/DigitalDisplays.svelte
+++ src/components/_Clock/DigitalDisplays.svelte
@@ -38,63 +38,55 @@
 </script>
 
 <div class="flex">
-	<div
+	<h1
 		id="primary-display"
 		style="--primary-font-weight: {$settings.clock
 			.datetimeFontWeight}; color:{displays.primaryPalette === 'base'
 			? baseColorPalette[shade]
 			: accentColorPalette[shade]}"
 		class={displays.primary + ' text-' + displays.primaryFontSize}>
-		{#if displays.primary != 'analog'}
-			<h1>
-				{#if displays.primary == 'time'}
-					{$time}
-				{:else if displays.primary == 'date'}
-					{$date}
-				{:else if displays.primary == 'datetime'}
-					{$time}
-					<br />
-					{$date}
-				{/if}
-			</h1>
+		{#if displays.primary == 'time'}
+			{$time}
+		{:else if displays.primary == 'date'}
+			{$date}
+		{:else if displays.primary == 'datetime'}
+			{$time}
+			<br />
+			{$date}
 		{/if}
-	</div>
-	<div
+	</h1>
+	<h2
 		id="secondary-display"
 		style="color:{displays.secondaryPalette === 'base'
 			? baseColorPalette[shade]
 			: accentColorPalette[shade]}"
-		class={'text-' + displays.secondaryFontSize}>
-		{#if displays.secondary != 'none'}
-			<h2>
-				{#if displays.secondary == 'time'}
-					{$time}
-				{:else if displays.secondary == 'date'}
-					{$date}
-				{:else if displays.secondary == 'datetime'}
-					{$time}
-					<br />
-					{$date}
-				{/if}
-			</h2>
+		class={'m-0 text-' + displays.secondaryFontSize}>
+		{#if displays.secondary == 'time'}
+			{$time}
+		{:else if displays.secondary == 'date'}
+			{$date}
+		{:else if displays.secondary == 'datetime'}
+			{$time}
+			<br />
+			{$date}
 		{/if}
-	</div>
+	</h2>
 </div>
 
 {#if displays.battery && batterySupported}
-	<div
+	<h2
 		id="battery-display"
-		class={'hidden sm:block text-' + displays.secondaryFontSize}
+		class={'m-0 hidden sm:block text-' + displays.secondaryFontSize}
 		style="color:{displays.secondaryPalette === 'base'
 			? baseColorPalette[shade]
 			: accentColorPalette[shade]}">
 		<BatteryIcon
 			fillLevel={batteryLevel ? batteryLevel * 100 : 100}
 			charging={batteryIsCharging}
-			class="inline w-6 h-6 md:w-8 md:h-8 mb-8" />
+			class="inline w-6 h-6 md:w-8 md:h-8" />
 
-		<h2 class="inline">{Math.round(batteryLevel * 100)}%</h2>
-	</div>
+		{Math.round(batteryLevel * 100)}%
+	</h2>
 {/if}
 
 <style lang="postcss">
@@ -103,30 +95,30 @@
 		font-weight: var(--primary-font-weight);
 	}
 
-	#primary-display h1 {
+	#primary-display {
 		@apply tracking-widest text-center leading-snug px-8;
 	}
 
-	#primary-display.text-small h1 {
+	#primary-display.text-small {
 		@apply text-6xl md:text-7xl lg:text-8xl xl:text-9xl;
 		@apply supershort:text-4xl short:text-5xl supershort:md:text-6xl short:lg:text-7xl;
 	}
-	#primary-display h1 {
+	#primary-display {
 		@apply text-7xl md:text-8xl lg:text-9xl xl:text-10xl;
 		@apply supershort:text-5xl short:text-6xl supershort:md:text-7xl short:lg:text-8xl;
 	}
-	#primary-display.text-large h1 {
+	#primary-display.text-large {
 		@apply text-8xl md:text-9xl lg:text-10xl xl:text-11xl;
 		@apply supershort:text-6xl short:text-7xl supershort:md:text-8xl short:lg:text-9xl;
 	}
 
-	#primary-display.datetime.text-small h1 {
+	#primary-display.datetime.text-small {
 		@apply text-4xl md:text-5xl lg:text-6xl;
 	}
-	#primary-display.datetime h1 {
+	#primary-display.datetime {
 		@apply text-5xl md:text-6xl lg:text-7xl;
 	}
-	#primary-display.datetime.text-large h1 {
+	#primary-display.datetime.text-large {
 		@apply text-6xl md:text-7xl lg:text-8xl;
 	}
 
@@ -139,31 +131,28 @@
 
 		white-space: break-spaces;
 	}
-	#secondary-display {
-		@apply pb-4;
-	}
 
-	#secondary-display h2,
-	#battery-display h2 {
+	#secondary-display,
+	#battery-display {
 		@apply font-normal tracking-widest text-center;
 	}
 
-	#secondary-display.text-small h2,
-	#battery-display.text-small h2 {
+	#secondary-display.text-small,
+	#battery-display.text-small {
 		@apply text-xl md:text-2xl lg:text-3xl;
 	}
-	#secondary-display h2,
-	#battery-display h2 {
+	#secondary-display,
+	#battery-display {
 		@apply text-2xl md:text-3xl lg:text-4xl;
 	}
-	#secondary-display.text-large h2,
-	#battery-display.text-large h2 {
+	#secondary-display.text-large,
+	#battery-display.text-large {
 		@apply text-3xl md:text-4xl lg:text-5xl;
 	}
 
 	#secondary-display,
 	#battery-display {
-		@apply absolute bottom-0 supershort:opacity-0;
+		@apply absolute bottom-4 supershort:opacity-0;
 	}
 	#secondary-display {
 		@apply left-0 w-full;
