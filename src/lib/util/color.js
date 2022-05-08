d5f21bca737b98c2e6ba292a0c4228df9a20262a
Justin Golden <justingolden21@gmail.com>
2022-05-08 16:24:22 -0700

    Update color.js

--- src/lib/util/color.js
+++ src/lib/util/color.js
@@ -1,18 +1,22 @@
 import TailwindColors from 'tailwindcss/colors';
 import { get } from 'svelte/store';
-import { settings } from '$lib/stores/settings';
+import { settings } from '$lib/stores/settings.js';
 
 // https://stackoverflow.com/a/5624139/4907950
-export function hexToRgb(hex) {
+export const hexToRgb = (hex) => {
 	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
-}
+};
 
-// return the hex color given a string or object with color information from a theme
-// lightness is a string (100, 200, 300) that represents the lightness of the color in the tailwind theme
-// if falsey or '-1', return 'none' (lack of value or '-1' results in a transparent color)
-// palette is 'base' unless otherwise specified, 'accent' is the other valid color palette
-export function getColor(obj) {
-	const palette = obj.palette === 'accent' ? 'accentColorPalette' : 'baseColorPalette';
-	return obj.lightness === '-1' ? 'none' : TailwindColors[get(settings)[palette]][obj.lightness];
-}
+/**
+ * Given a color object, return hex color
+ * @param {object} obj: color information object from a theme
+ * @param {string} lightness: a string (100, 200, 300) that represents the lightness of the color in the tailwind theme
+ * if -1, return 'none' (transparent)
+ * @param {string} palette: 'base' unless otherwise specified, 'accent' is the other valid color palette
+ * @returns hex color string or 'none'
+ */
+export const getColor = (obj) =>
+	obj.lightness === '-1'
+		? 'none'
+		: TailwindColors[get(settings)[`${obj.palette}ColorPalette`]][obj.lightness];
