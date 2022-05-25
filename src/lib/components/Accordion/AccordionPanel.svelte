b07a8730b33b929cdfe7eb1f543742f66354bc26
Justin Golden <justingolden21@gmail.com>
2022-05-24 21:51:39 -0700

    accordion panel css refactor

--- src/lib/components/Accordion/AccordionPanel.svelte
+++ src/lib/components/Accordion/AccordionPanel.svelte
@@ -23,10 +23,7 @@
 </script>
 
 <div class="details">
-	<button
-		class="w-full text-left cursor-pointer p-2 hover:bg-base-50 focus:bg-base-100 font-normal text-lg dark:hover:bg-base-600 dark:focus:bg-base-600 bg-opacity-25 dark:bg-base-700 dark:bg-opacity-50"
-		on:click={handleToggle}
-		aria-expanded={params.open}>
+	<button class="details__summary" on:click={handleToggle} aria-expanded={params.open}>
 		<Icon
 			name="chevron_right"
 			class="inline w-6 h-6 transition-transform {params.open ? 'rotate-90' : ''}"
@@ -34,7 +31,7 @@
 		{accordionTitle}
 	</button>
 
-	<div class="details-content" class:open={params.open}>
+	<div class="details__content" class:open={params.open}>
 		<slot />
 	</div>
 </div>
@@ -43,10 +40,22 @@
 	.details {
 		@apply bg-base-50 border-b-2 border-base-200 bg-opacity-25;
 	}
-	.details .details-content {
-		@apply p-4 hidden dark:bg-base-700 dark:bg-opacity-50;
+	/* prevent white flash when opening details in dark mode */
+	:global(.dark) .details {
+		@apply bg-base-700 bg-opacity-25;
 	}
-	.details-content.open {
+
+	.details__summary {
+		@apply w-full text-left cursor-pointer p-2 hover:bg-base-50 focus:bg-base-100 font-normal text-lg bg-opacity-25;
+	}
+	:global(.dark) .details__summary {
+		@apply hover:bg-base-600 focus:bg-base-600 bg-base-700 bg-opacity-50;
+	}
+
+	.details__content {
+		@apply p-4 hidden;
+	}
+	.details__content.open {
 		@apply block;
 		animation: sweep 0.5s ease-in-out;
 	}
@@ -60,10 +69,5 @@
 			opacity: 1;
 			transform: translateX(0);
 		}
-	}
-
-	/* prevent white flash when opening details in dark mode */
-	:global(.dark) .details {
-		@apply bg-base-700 bg-opacity-25;
 	}
 </style>
