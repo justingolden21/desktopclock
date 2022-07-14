32308365c52238708a1699e9bad2da7bae6b0404
Justin Golden <justingolden21@gmail.com>
2022-07-13 23:03:26 -0700

    Update packages and postcss config
    Added line to js config to fix build warning

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,20 +1,6 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const postcssimport = require('postcss-import');
-
-const dev = process.env.NODE_ENV === 'development';
-
 module.exports = {
-	plugins: [
-		// postcss needs to run before tailwind
-		postcssimport(),
-		tailwindcss(),
-		// autoprefixer needs to run after tailwind
-		autoprefixer(),
-		!dev &&
-			cssnano({
-				preset: 'default'
-			})
-	]
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {}
+	}
 };
