554b3e6f4f300c481218916320bb123791f9fd2b
Justin Golden <justingolden21@gmail.com>
2022-07-13 22:59:40 -0700

    Config and readme cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,8 +3,7 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcssimport = require('postcss-import');
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = process.env.NODE_ENV === 'development';
 
 module.exports = {
 	plugins: [
