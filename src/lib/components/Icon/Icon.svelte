f96b853f9841d18396f691e7f68e244a7f7ea451
Justin Golden <justingolden21@gmail.com>
2022-07-23 00:11:37 -0700

    Cleanup Icon.svelte

--- src/lib/components/Icon/Icon.svelte
+++ src/lib/components/Icon/Icon.svelte
@@ -34,8 +34,7 @@
 			path: `M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z`
 		},
 		'sun': {
-			fill: true,
-			path: `m12 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zm1.344 6.657a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zm-4.95-8.536a1 1 0 101.415-1.414l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zm-2.464-3.95a1 1 0 100-2h-1a1 1 0 000 2h1z`
+			path: `M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z`
 		},
 		'fullscreen': {
 			style: 'square',
@@ -147,9 +146,9 @@
 </script>
 
 <svg
-	class={$$props.class + ` icon-${name}`}
-	stroke={icon.fill ? 'none' : 'currentColor'}
-	fill={icon.fill ? 'currentColor' : 'none'}
+	class={($$props.class ?? 'w-6 h-6 inline') + ` icon-${name}`}
+	stroke="currentColor"
+	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
 	xmlns:xlink="http://www.w3.org/1999/xlink"
 	viewBox={icon.viewbox ?? '0 0 24 24'}>
